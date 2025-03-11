a0d78734977363bd02a4512bd4379f398cfafcfc
ozerikoz <erik.paschoalatto1@gmail.com>
2025-03-11 12:00:35 -0300

    fix erro no componente Resultado

--- components/Resultado.js
+++ components/Resultado.js
@@ -5,7 +5,7 @@ export default function ResultadoAumentoProduto({valorOriginalProduto, aumentoPe
     <Text>Resultado:</Text>
     <Text>Valor original do produto: R${valorOriginalProduto}</Text>
     <Text>O percentual de aumento do produto: {aumentoPercentualProduto}%</Text>
-    <Text>O novo valor do produto: {novoValorProduto}</Text>
-    <Text>O Valor de aumento do produto: + R${novoValorProduto-valorOriginalProduto.toFixed(2)}</Text>
+    <Text>O novo valor do produto: R${novoValorProduto}</Text>
+    <Text>O Valor de aumento do produto: + R${novoValorProduto-valorOriginalProduto}</Text>
  </View>
 }
