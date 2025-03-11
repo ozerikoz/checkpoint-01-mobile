f6ad8d28fb8d64dbba0f6723b2f33c464d7bf7eb
ozerikoz <erik.paschoalatto1@gmail.com>
2025-03-11 11:39:24 -0300

    add formulario do calculo de aumento do produto

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import CalcularAumentoProduto from './components/Calcular_Aumento_Produto';
+import ResultadoAumentoProduto from './components/Resultado';
 
 export default function App() {
+  const [nomeProduto, setNomeProduto] = useState('');
+  const [valorProduto, setValorProduto] = useState(0.0);
+  const [aumentoPercentualProduto, setAumentoPercentualProduto] = useState(0.0);
+  const [valorResultado, setValorResultado] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Calculadora de porcentagem</Text>
+      <TextInput onChangeText={setNomeProduto} value={nomeProduto} placeholder='Digite o nome do produto' keyboardType='text'/>
+      <TextInput onChangeText={setValorProduto} value={valorProduto} placeholder='Digite o valor original do produto' keyboardType='numeric'/>
+      <TextInput onChangeText={setAumentoPercentualProduto} value={aumentoPercentualProduto} placeholder='Digite a porcentagem% de aumento do produto' keyboardType='numeric'/> 
+      <Button title='Calcular' onPress={()=>{setValorResultado(CalcularAumentoProduto(valorProduto, aumentoPercentualProduto))}}/>
+    { (valorResultado == null) ? <></> : <ResultadoAumentoProduto valorOriginalProduto={valorProduto} aumentoPercentualProduto={aumentoPercentualProduto} novoValorProduto={valorResultado} />}
     </View>
   );
 }
